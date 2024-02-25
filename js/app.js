31be3d48979e7878ea2366509a59a2306900095c
Biplob <biplobariya4@gmail.com>
2024-02-25 22:23:24 +0600

    working update

--- js/app.js
+++ js/app.js
@@ -1,23 +1,48 @@
-var trigger = document.querySelector(".menutrigger1");
+
+
+var menutrigger = document.querySelector(".menutrigger1");
 var menu = document.querySelector(".menu");
 
-trigger.addEventListener("click", function(){
+menutrigger.addEventListener("click", function(){
     menu.classList.toggle("active");
-    this.classList.toggle("active");
+    menutrigger.classList.toggle("active");
 });
 
 
-
-// second-menu
-var trigger2 = document.querySelector(".menutrigger2");
+// second-menu script
+var menutrigger2 = document.querySelector(".menutrigger2");
 var secmenu = document.querySelector(".sec-menu");
 
-trigger2.addEventListener("click", function(){
+menutrigger2.addEventListener("click", function(){
     secmenu.classList.toggle("active2");
-    this.classList.toggle("active2");
+    menutrigger2.classList.toggle("active2");
+    
 });
+// Close sidebar menu when click outside
 
 
+// Search Popup
+var overlay =document.getElementById('search-overlay');
+var searchPopup =document.getElementById('search-popup');
+
+function openOverlay(){
+  overlay.classList.add("active-overlay");
+}
+function closeOverlay(){
+  overlay.classList.remove("active-overlay");
+}
+
+
+function openPopup(){
+  searchPopup.classList.add("open-popup");
+}
+
+function closePopup(){
+  searchPopup.classList.remove("open-popup");
+}
+
+
+// Swiper Slider Script
 var swiper = new Swiper(".mySwiper", {
 
 
@@ -28,7 +53,20 @@ var swiper = new Swiper(".mySwiper", {
       return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
   },
-  // autoplay: {
-  //   delay: 4000,
-  // },
 });
+
+
+// Close sidebar menu when click outside
+document.onclick = function(e){
+  if(!menutrigger.contains(e.target) && !menu.contains(e.target)){
+    menu.classList.remove("active");
+    menutrigger.classList.remove("active");
+  }
+  
+  if(!menutrigger2.contains(e.target) && !secmenu.contains(e.target)){
+    secmenu.classList.remove("active2");
+    menutrigger2.classList.remove("active2");
+  }
+  
+
+}
